6af61d7f9cabed8d2ecffdbabc26d9de72f764b5
Scott Robinson <srobins@Scotts-MacBook-Air.local>
2014-08-30 13:25:07 +1000

    First cut at installing my new work laptop.

---
+++ laptop.rb
@@ -0,0 +1,37 @@
+dep 'laptop' do
+  BREWS.each { |c| requires "#{c}.managed" }
+  CASKS.each { |c| requires "#{c}.cask" }
+  requires 'alfred cask integration'
+end
+
+BREWS=%w{
+  ack
+}
+
+CASKS=%w{
+  airfoil
+  alfred
+  box-sync
+  caffeine
+  geektool
+  google-chrome
+  iterm2
+  macvim
+  onepassword
+  skype
+  slack
+  sourcetree
+  spectacle
+  the-unarchiver
+  vagrant
+  virtualbox
+  vlc
+}
+
+BREWS.each do |cask|
+  dep "#{cask}.managed"
+end
+
+CASKS.each do |cask|
+  dep "#{cask}.cask"
+end
