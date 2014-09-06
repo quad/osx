69a701d58e15833626b55974a93c373847b6cd71
Scott Robinson <srobins@thoughtworks.com>
2014-09-06 10:16:32 +1000

    Fix brew and cask updates.

--- laptop.rb
+++ laptop.rb
@@ -1,9 +1,11 @@
-dep 'laptop' do
-  brews = File.readlines(File.expand_path '../brews.lst', __FILE__)
-  brews.each { |c| requires dep("#{c.strip}.managed") }
+brews = File.readlines(File.expand_path '../brews.lst', __FILE__)
+brews.each { |c| dep "#{c.strip}.managed" }
+casks = File.readlines(File.expand_path '../casks.lst', __FILE__)
+casks.each { |c| dep "#{c.strip}.cask" }
 
-  casks = File.readlines(File.expand_path '../casks.lst', __FILE__)
-  casks.each { |c| requires dep("#{c.strip}.cask") }
+dep 'laptop' do
+  brews.each { |c| requires "#{c.strip}.managed" }
+  casks.each { |c| requires "#{c.strip}.cask" }
   requires 'alfred cask integration'
 
   requires 'sporkd:osx computer name set'.with computer_name: 'rabi', local_hostname: 'rabi'
