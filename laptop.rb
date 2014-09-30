dda3608554f52cfd9cbd9f117b000846f973ec95
Scott Robinson <srobins@thoughtworks.com>
2014-09-30 21:19:44 +1000

    Allow brews and casks to say what binaries they're installing, if necessary.

--- laptop.rb
+++ laptop.rb
@@ -1,11 +1,15 @@
-brews = File.readlines(File.expand_path '../brews.lst', __FILE__)
-brews.each { |c| dep "#{c.strip}.managed" }
-casks = File.readlines(File.expand_path '../casks.lst', __FILE__)
-casks.each { |c| dep "#{c.strip}.cask" }
+parse = -> (l) {
+  n, p = l.strip.split(':')
+  [n, -> (t) { dep "#{n}.#{t}" do provides p if p end }]
+}
+brews = File.readlines(File.expand_path '../brews.lst', __FILE__).map &parse
+casks = File.readlines(File.expand_path '../casks.lst', __FILE__).map &parse
+brews.each { |n, p| p['managed'] }
+casks.each { |n, p| p['cask'] }
 
 dep 'laptop' do
-  brews.each { |c| requires "#{c.strip}.managed" }
-  casks.each { |c| requires "#{c.strip}.cask" }
+  brews.each { |n, p| requires "#{n}.managed" }
+  casks.each { |n, p| requires "#{n}.cask" }
   requires 'alfred cask integration'
 
   requires 'sporkd:osx computer name set'.with computer_name: 'rabi', local_hostname: 'rabi'
