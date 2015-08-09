e5559b2792c52e1f8800abfa180d501144ab0a75
Scott Robinson <scott@quadhome.com>
2015-08-09 13:37:28 +1000

    Remove obsolete alfred integration

--- homebrew/deps/homebrew-cask.rb
+++ homebrew/deps/homebrew-cask.rb
@@ -4,10 +4,3 @@ dep 'homebrew-cask' do
   meet { shell 'brew install caskroom/cask/brew-cask' }
   met? { shell? 'brew cask' }
 end
-
-dep 'alfred cask integration' do
-  requires 'alfred.cask'
-
-  meet { shell 'brew cask alfred link' }
-  met? { `brew cask alfred status`.include? 'is happily linked' }
-end
