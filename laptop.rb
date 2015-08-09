238812726a24d4bedc34902119af4865c66d5042
Scott Robinson <scott@quadhome.com>
2015-08-09 13:37:39 +1000

    Update for new hostname.

--- laptop.rb
+++ laptop.rb
@@ -11,7 +11,7 @@ dep 'laptop' do
   brews.each { |n, p| requires "#{n}.managed" }
   casks.each { |n, p| requires "#{n}.cask" }
 
-  requires 'sporkd:osx computer name set'.with computer_name: 'nathalia', local_hostname: 'nathalia'
+  requires 'sporkd:osx computer name set'.with computer_name: 'qian', local_hostname: 'qian'
 
   requires 'gitconfig'
   requires 'vim'
